9f9a67052a95320543822fc072255bbe1429398e
losingthepicture <dragonslayer817@hotmail.com>
2022-08-04 12:05:33 -0500

    02 pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let str= "";
+    if (num < 0) {
+        return "ERROR"
+    }
+  for (i = 0; i < num; i++) {
+    str = str.concat(string);
+  }
+  return str;
 };
 
 // Do not edit below this line
