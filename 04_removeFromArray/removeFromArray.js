438c6017d15dd355d2f7e8c4d722fa5c92f7b7ae
losingthepicture <dragonslayer817@hotmail.com>
2022-08-05 16:37:37 -0500

    finally getting somewhere

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,15 @@
 const removeFromArray = function (array, ...remove) {
   let concatNum = [];
   let fn = [];
-  for (const arrayValue of remove) {
-    if (typeof arrayValue === "string") {
-      const filteredString = array.filter(
-        (removeString) => removeString != arrayValue
-      );
-    } else if (typeof arrayValue === "number") {
-      const filteredNumber = array.filter(
-        (removeNumber) => removeNumber != arrayValue
-      );
-      //console.log(filteredNumber);
-      //console.log(concatNum = concatNum.concat(arrayValue));
-      console.log(filteredNumber);
-      // if the value in the array is a number. create a new array
-    }
-  }
+  let found = [];
   
+  
+  for (i = 0; i < remove.length; i++) {
+    console.log(array.indexOf(remove[i]));
+  }
+
 };
-console.log(removeFromArray([1, 2, 3, 4], 2, 3, 4));
+console.log(removeFromArray([1, 2, 3, 4], 3, 4));
 //console.log(removeFromArray(["yes", "no", "epic"], "yes"));
 // Do not edit below this line
 module.exports = removeFromArray;
