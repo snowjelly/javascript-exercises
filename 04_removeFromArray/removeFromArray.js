e3d2212718e99ffeb28720c9dfa32a087dd47bd8
losingthepicture <dragonslayer817@hotmail.com>
2022-08-05 15:15:08 -0500

    stuck

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,25 +1,23 @@
-const removeFromArray = function (ar, ...remove) {
-  let removed = [];
-  let iter = 0;
-
-  console.log("ar: " + ar);
-  console.log("remove: " + remove);
-  //'remove' is what needs to be removed.
-  //i need a loop to search the array for 'remove'
-  //splice(where to remove element, how many)
-  for (let i = 0; i < ar.length; i++) {
-    if (ar[i] === remove[iter]) {
-      removed = ar.splice(i, 1);
-      console.log("iter: " + iter);
-      console.log("remove length " + remove.length);
-    }
-    console.log("i: " + i + " \n removed: " + removed + "\n iter: " + iter);
-    if (iter < remove.length - 1) {
-        iter++;
+const removeFromArray = function (array, ...remove) {
+  let concatNum = [];
+  let fn = [];
+  for (const arrayValue of remove) {
+    if (typeof arrayValue === "string") {
+      const filteredString = array.filter(
+        (removeString) => removeString != arrayValue
+      );
+    } else if (typeof arrayValue === "number") {
+      const filteredNumber = array.filter(
+        (removeNumber) => removeNumber != arrayValue
+      );
+      //console.log(filteredNumber);
+      //console.log(concatNum = concatNum.concat(arrayValue));
+      console.log(concatNum = concatNum.concat(filteredNumber));
     }
   }
-  return ar;
+  
 };
-  console.log(removeFromArray([1, 2, 3, 4], 2, 3));
-  // Do not edit below this line
-  module.exports = removeFromArray;
+console.log(removeFromArray([1, 2, 3, 4], 2, 3, 4));
+//console.log(removeFromArray(["yes", "no", "epic"], "yes"));
+// Do not edit below this line
+module.exports = removeFromArray;
