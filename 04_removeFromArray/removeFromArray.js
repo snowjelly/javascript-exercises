e7aad9a35d3cab2d8bdf8057b27c1ac4d5fa4fe1
losingthepicture <dragonslayer817@hotmail.com>
2022-08-05 13:32:15 -0500

    stuck

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function (ar, ...remove) {
+  let removed = [];
+  let iter = 0;
 
+  console.log("ar: " + ar);
+  console.log("remove: " + remove);
+  //'remove' is what needs to be removed.
+  //i need a loop to search the array for 'remove'
+  //splice(where to remove element, how many)
+  for (let i = 0; i < ar.length; i++) {
+    if (ar[i] === remove[iter]) {
+      removed = ar.splice(i, 1);
+      console.log("iter: " + iter);
+      console.log("remove length " + remove.length);
+    }
+    console.log("i: " + i + " \n removed: " + removed + "\n iter: " + iter);
+    if (iter < remove.length - 1) {
+        iter++;
+    }
+  }
+  return ar;
 };
-
-// Do not edit below this line
-module.exports = removeFromArray;
+  console.log(removeFromArray([1, 2, 3, 4], 2, 3));
+  // Do not edit below this line
+  module.exports = removeFromArray;
