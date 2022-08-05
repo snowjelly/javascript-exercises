a235e060930094f14e18601c077102d9dda5210d
losingthepicture <dragonslayer817@hotmail.com>
2022-08-05 17:24:47 -0500

    hmmm

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,25 @@
 const removeFromArray = function (array, ...remove) {
   let concatNum = [];
   let fn = [];
-  let found = [];
   let int = 0;
+  let lengthStore = array.length;
 
-  for (i = 0; i < remove.length; i++) {
-    int = found.push(array.indexOf(remove[i]));
-  }
-  
-  
+function checkIndex() {
+    let found = [];
+    for (i = 0; i < remove.length; i++) {
+        int = found.push(array.indexOf(remove[i]));
+      }
+      return found;
+}
+
+
+
+  for (removal of checkIndex()) {
+      array.splice(removal, 1);
+      console.log("array: " + array);
+      console.log("checkIndex: " + checkIndex());
+    }
+
 
 };
 console.log(removeFromArray([1, 2, 3, 4], 3, 4));
