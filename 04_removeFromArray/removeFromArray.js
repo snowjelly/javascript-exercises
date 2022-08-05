83b825662a79c6287e4556ee825ce2889acbbb46
losingthepicture <dragonslayer817@hotmail.com>
2022-08-05 16:56:30 -0500

    for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,14 +2,17 @@ const removeFromArray = function (array, ...remove) {
   let concatNum = [];
   let fn = [];
   let found = [];
-  
-  
+  let int = 0;
+
   for (i = 0; i < remove.length; i++) {
-    console.log(array.indexOf(remove[i]));
+    int = found.push(array.indexOf(remove[i]));
   }
+  
+  
 
 };
 console.log(removeFromArray([1, 2, 3, 4], 3, 4));
+
 //console.log(removeFromArray(["yes", "no", "epic"], "yes"));
 // Do not edit below this line
 module.exports = removeFromArray;
