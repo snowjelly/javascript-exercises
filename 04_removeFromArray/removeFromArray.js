e4a1e123cb7a39ab62e27464a62fc31340fec70e
losingthepicture <dragonslayer817@hotmail.com>
2022-08-06 16:29:13 -0500

    formatted

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,66 +2,54 @@ const removeFromArray = function (arrayKeep, ...arrayRemove) {
   const args = Array.from(arrayRemove);
   let newArray = [];
 
-
   //removeIndex() creates an array called 'newArray'
   //'newArray' takes the indexOf every individual element inside 'args'
-const removeIndex = () => {
-  for (i = 0; i < args.length; i++) {
-    if (arrayKeep.includes(args[i])) {
-      newArray.push(arrayKeep.indexOf(args[i]));
-    } else {
-      //args is null
-      return null;
+  const removeIndex = () => {
+    for (i = 0; i < args.length; i++) {
+      if (arrayKeep.includes(args[i])) {
+        newArray.push(arrayKeep.indexOf(args[i]));
+      } else {
+        //args is null
+        return null;
+      }
     }
-  }
-  return newArray;
-}
-
-
-
-
+    return newArray;
+  };
 
   //logs variables before result gets returned
   const logPreResult = () => {
-  //args is 'arrayRemove' converted to Array object
-  //removeIndex is an array displaying the index of items found in arrayKeep that need to be removed 
-  console.log("remove values: " + args, "=> " + "the values were found in indicies: " + removeIndex());
-  }
+    //args is 'arrayRemove' converted to Array object
+    //removeIndex is an array displaying the index of items found in arrayKeep that need to be removed
+    console.log(
+      "remove values: " + args,
+      "=> " + "the values were found in indicies: " + removeIndex()
+    );
+  };
   logPreResult();
 
   let iter = 0;
   //loop and filter out whatever we return. el is essentially result[i] and index is "i"
-  const result = arrayKeep.filter((el, index) => { 
-  
+  const result = arrayKeep.filter((el, index) => {
     if (removeIndex() === null) {
       return arrayKeep;
     }
     // el is result[i]
     // index is 'i'
     //loop through the arguments that need to be removed to find what we need to return to filter out
-    
-
-        
-      console.log("testing removal of value: " + args[iter], "on element: " + el, "in indicies => " + "arrayKeep: " + index + " args: " + iter);
-      
-
-
-      
-      //return
-      if (el != args[iter]) {
-        return el;
-      }
-      else {
-        console.log("value " + args[iter] + " was removed succesfully");
-        iter++;
-      }
-    
-    
+    console.log(
+      "testing removal of value: " + args[iter],
+      "on element: " + el,
+      "in indicies => " + "arrayKeep: " + index + " args: " + iter
+    );
+    //return
+    if (el != args[iter]) {
+      return el;
+    } else {
+      console.log("value " + args[iter] + " was removed succesfully");
+      iter++;
+    }
   });
   return result;
-
-
-
 };
 console.log(removeFromArray([1, 2, 3, 4], 1, 4));
 
