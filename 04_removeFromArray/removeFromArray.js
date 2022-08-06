f290a531f2c41f622406be5ce1ccf74dab6b6282
losingthepicture <dragonslayer817@hotmail.com>
2022-08-06 13:53:46 -0500

    added comments and better logging

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,11 +3,9 @@ const removeFromArray = function (arrayKeep, ...arrayRemove) {
   let newArray = [];
 
 
-  //for (count of args) {
-  //console.log("count: " + count);
-  //}
-  console.log("args: " + args);
 
+  //removeIndex() creates an array called 'newArray'
+  //'newArray' takes the indexOf every individual element inside 'args'
 const removeIndex = () => {
   for (i = 0; i < args.length; i++) {
     if (arrayKeep.includes(args[i])) {
@@ -16,23 +14,39 @@ const removeIndex = () => {
   }
   return newArray;
 }
-console.log("remove index: " + removeIndex());
+
+  //logs variables before result gets returned
+  const logPreResult = () => {
+  //args is 'arrayRemove' converted to Array object
+  //removeIndex is an array displaying the index of items found in arrayKeep that need to be removed 
+  console.table(["remove values: " + args, "values found in indicies: " + removeIndex()]);
+  }
+  logPreResult();
 
 
-  const result = arrayKeep.filter((el, index) => { //loop and filter out whatever we return. el is essentially result[i] and index is "i"
-    for (value of args) { //loop through the arguments that need to be removed to find what we need to return to filter and create the new array
-       console.log("value: "+ value);
+  //loop and filter out whatever we return. el is essentially result[i] and index is "i"
+  const result = arrayKeep.filter((el, index) => { 
+    //loop through the arguments that need to be removed to find what we need to return to filter out
+    for (value of args) { 
+      // value = args[i]
+      console.log("value: "+ value);
+      
+
+      // el is result[index] 
       // console.log("el: " + el);
+      
+
+      // index is 'i'
       // console.log("index: " + index);
 
       
-        
+      //return
       if (el != value) {
         return el != value;
       }
     }
 
-    // console.log(args[index]);
+    
     
   });
   return result;
@@ -51,7 +65,7 @@ console.log("remove index: " + removeIndex());
   //const result = arrayKeep.filter((element, index, array) => element); //since .filter loops through arrayKeep maybe i can ask it to check if it includes the values
   //i need AND returns the correct values before creating the new array
 };
-console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3, 4], 2, 3));
 
 //console.log(removeFromArray(["yes", "no", "epic"], "yes"));
 // Do not edit below this line
