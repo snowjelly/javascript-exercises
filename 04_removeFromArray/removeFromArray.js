be00f28e92d0089edbaa6a6eff3df0fa3b9152b1
losingthepicture <dragonslayer817@hotmail.com>
2022-08-06 09:42:24 -0500

    restart

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,34 +1,12 @@
 const removeFromArray = function (arrayKeep, ...arrayRemove) {
-  const args = Array.from(arrayRemove);
-  let searchIndex = [];
-
   
-const createSearchIndex = function () {
-    for (i = 0; i < arrayRemove.length; i++) {
-        searchIndex.push(arrayKeep.indexOf(args[i]));
-    }
-    return console.log(searchIndex);
-}
-
-
-const killSearchIndex = function () {
-  return console.log(searchIndex = []);
-}
-
-
-for (i = 0; i < arrayKeep.length; i++) {
-    if (arrayKeep.includes(args[i])) {
-        createSearchIndex();
-    }
-}
-
-for (i = 0; i < createSearchIndex.length; i++) {
-    return arrayKeep.filter((el) => el.includes)
-}
 
 
 };
-console.log(removeFromArray([1, 2, 3, 4], 3, 4));
+
+
+
+
 
 //console.log(removeFromArray(["yes", "no", "epic"], "yes"));
 // Do not edit below this line
