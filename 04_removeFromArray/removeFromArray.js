9f261336d8a394402cb4c13315314981e3641e02
losingthepicture <dragonslayer817@hotmail.com>
2022-08-06 14:27:15 -0500

    works without multiple args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,17 +3,23 @@ const removeFromArray = function (arrayKeep, ...arrayRemove) {
   let newArray = [];
 
 
-
   //removeIndex() creates an array called 'newArray'
   //'newArray' takes the indexOf every individual element inside 'args'
 const removeIndex = () => {
   for (i = 0; i < args.length; i++) {
     if (arrayKeep.includes(args[i])) {
       newArray.push(arrayKeep.indexOf(args[i]));
+    } else {
+      //args is null
+      return null;
     }
   }
   return newArray;
 }
+
+
+
+
 
   //logs variables before result gets returned
   const logPreResult = () => {
@@ -26,11 +32,15 @@ const removeIndex = () => {
 
   //loop and filter out whatever we return. el is essentially result[i] and index is "i"
   const result = arrayKeep.filter((el, index) => { 
+    if (removeIndex() === null) {
+      return arrayKeep;
+    }
     //loop through the arguments that need to be removed to find what we need to return to filter out
+    // el is result[i]
+    // index is 'i'
     for (value of args) { 
       // value is args[i]
-      // el is result[i]
-      // index is 'i'
+
        console.log("testing removal of value: "+ value, "on element: " + el, "in index: " + index);
       
 
@@ -64,7 +74,7 @@ const removeIndex = () => {
   //const result = arrayKeep.filter((element, index, array) => element); //since .filter loops through arrayKeep maybe i can ask it to check if it includes the values
   //i need AND returns the correct values before creating the new array
 };
-console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3, 4], 2));
 
 //console.log(removeFromArray(["yes", "no", "epic"], "yes"));
 // Do not edit below this line
