e6edb6059f00a9c3fce7a0ae1811337331ee0750
losingthepicture <dragonslayer817@hotmail.com>
2022-08-06 12:57:15 -0500

    hmm

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,21 +2,33 @@ const removeFromArray = function (arrayKeep, ...arrayRemove) {
   const args = Array.from(arrayRemove);
   let newArray = [];
 
+
   //for (count of args) {
   //console.log("count: " + count);
   //}
   console.log("args: " + args);
 
-  const fruits = ["apple", "banana", "grapes", "mango", "orange"];
+const removeIndex = () => {
+  for (i = 0; i < args.length; i++) {
+    if (arrayKeep.includes(args[i])) {
+      newArray.push(arrayKeep.indexOf(args[i]));
+    }
+  }
+  return newArray;
+}
+console.log("remove index: " + removeIndex());
 
-  /**
-   * Filter array items based on search criteria (query)
-   */
 
-  const result = arrayKeep.filter((el, index) => {
-    for (value of args) {
-      if (args[index]) {
-        return el != args[index];
+  const result = arrayKeep.filter((el, index) => { //loop and filter out whatever we return. el is essentially result[i] and index is "i"
+    for (value of args) { //loop through the arguments that need to be removed to find what we need to return to filter and create the new array
+       console.log("value: "+ value);
+      // console.log("el: " + el);
+      // console.log("index: " + index);
+
+      
+        
+      if (el != value) {
+        return el != value;
       }
     }
 
@@ -24,6 +36,7 @@ const removeFromArray = function (arrayKeep, ...arrayRemove) {
     
   });
   return result;
+
 
   // const timeToFilter = (value) => { //this piece of shit is not looping. it just returns on the first value it gets.
   //   //how the fuck do i filter an array without just returning the first value
@@ -38,7 +51,7 @@ const removeFromArray = function (arrayKeep, ...arrayRemove) {
   //const result = arrayKeep.filter((element, index, array) => element); //since .filter loops through arrayKeep maybe i can ask it to check if it includes the values
   //i need AND returns the correct values before creating the new array
 };
-console.log(removeFromArray([1, 2, 3, 4], 2, 3));
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 //console.log(removeFromArray(["yes", "no", "epic"], "yes"));
 // Do not edit below this line
