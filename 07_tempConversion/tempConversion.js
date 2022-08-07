f1714a8753497f08ec7a3df907b8c1312a146811
losingthepicture <dragonslayer817@hotmail.com>
2022-08-07 14:36:25 -0500

    gonna test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+const c = (f - 32) * 5/9;
+  return Math.floor(c);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  const f = c * 9/5 + 32;
+  return Math.floor(f);
 };
 
 // Do not edit below this line
