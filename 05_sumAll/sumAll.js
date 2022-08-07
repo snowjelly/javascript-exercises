c6d7daa85da204276c6fa2303a82261205251d44
losingthepicture <dragonslayer817@hotmail.com>
2022-08-06 20:42:24 -0500

    first 2 passes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+let sum = 0;
+let add = 0;
+for (i = start; i < end + 1; i++) {
+    // 1 + 2 + 3 + 4
+    add++;
+    console.log(add);
+    sum += add;
+}
 
+return sum;
 };
-
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
